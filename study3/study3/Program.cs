4fb21fe4da8146d2a757ee337960cf623a80d301
danseon <chlskrdnjs12@gmail.com>
2025-02-21 10:45:05 +0900

    study3

---
+++ study3/study3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace study3
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            //리터럴 : 코드에서 고정된 값을 의미한다.
+            int hp = 100;
+            double atk = 56.7;
+            char rank = 'S';
+            string name = "??";
+
+            Console.WriteLine($"hp :{hp}");
+            Console.WriteLine($"att : {atk}");
+            Console.WriteLine($"캐릭터 이름 :{name}");
+            Console.WriteLine($"등급 : {rank}");
+        }
+    }
+}
