02e0f8cd1b5e2c4518f00da6be933bda52ab6aa6
danseon <chlskrdnjs12@gmail.com>
2025-02-21 10:26:10 +0900

    day2-1

---
+++ LikeLion2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            //변수선언
+            int age;
+            age = 25;
+
+            Console.WriteLine(age);
+        }
+    }
+}
